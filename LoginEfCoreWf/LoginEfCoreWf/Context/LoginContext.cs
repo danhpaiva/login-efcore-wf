40c61a41e69b9543f59ca8b55c670d7e68628162
Paiva <danpaiva@live.com>
2025-04-09 20:28:56 -0300

    feat: add classe dbContext

---
+++ LoginEfCoreWf/LoginEfCoreWf/Context/LoginContext.cs
@@ -0,0 +1,21 @@
+﻿using LoginEfCoreWf.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginEfCoreWf.Context;
+
+public class LoginContext : DbContext
+{
+    public DbSet<Login> Logins { get; set; }
+
+    public string DbPath { get; }
+
+    public LoginContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "db_login.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
