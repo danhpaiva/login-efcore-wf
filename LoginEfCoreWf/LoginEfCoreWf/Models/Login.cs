9b31058aae2e595857c2c043c8129a9b9b3d3a57
Paiva <danpaiva@live.com>
2025-04-09 20:26:24 -0300

    feat: add classe login

---
+++ LoginEfCoreWf/LoginEfCoreWf/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginEfCoreWf.Models
+{
+    public class Login
+    {
+        public int LoginId { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
