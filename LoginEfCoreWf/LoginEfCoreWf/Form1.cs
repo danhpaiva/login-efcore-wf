e7b041d595e908d996ae049ae0519edd7a85f472
Paiva <danpaiva@live.com>
2025-04-09 20:50:21 -0300

    feat: add logica de cadastro e login ao mesmo tempo

--- LoginEfCoreWf/LoginEfCoreWf/Form1.cs
+++ LoginEfCoreWf/LoginEfCoreWf/Form1.cs
@@ -1,3 +1,6 @@
+using LoginEfCoreWf.Context;
+using LoginEfCoreWf.Models;
+
 namespace LoginEfCoreWf
 {
     public partial class f_login : Form
@@ -12,7 +15,19 @@ namespace LoginEfCoreWf
             //login: danielpaiva
             //senha: amo.corrida
 
-            
+            var login = tb_login.Text.ToString();
+            var senha = tb_senha.Text.ToString();
+
+            if (!String.IsNullOrEmpty(login) && !String.IsNullOrEmpty(senha))
+            {
+                using var db = new LoginContext();
+
+                db.Add(new Login { Nome = login, Senha = senha });
+                db.SaveChangesAsync();
+
+                MessageBox.Show("Tudo deu certo! Dados cadastrados!\nLogado no Sistema!");
+            } else
+                MessageBox.Show("Voce precisa preencher os dois campos!");
         }
     }
 }
